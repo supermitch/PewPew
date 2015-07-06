81200ff4c4e214d81c59b9bcc22812acb6358ae3
Mitch LeBlanc <supermitch@gmail.com>
2015-07-05 20:24:07 -0700

    Handle renderer in scene constructor

--- scenes/start.py
+++ scenes/start.py
@@ -3,7 +3,9 @@ from base import BaseScene
 class StartScene(BaseScene):
     """ Game intro screen. """
 
-    def __init__(self, win_surf, FPS):
-        super(StartScene, self).__init__(win_surf, FPS)
+    def __init__(self, renderer):
+        self.renderer = renderer
+        super(StartScene, self).__init__(self.renderer.surf, self.renderer.fps)
+
         self.surfaces = StartWorld(self.surf.get_size()).surfaces
 
