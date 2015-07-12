db1a6228e79facdae2d6a6f73319b7ca882fc46f
Mitch LeBlanc <supermitch@gmail.com>
2015-07-11 21:55:49 -0700

    Update constructor for GameOverScene

--- scenes/gameover.py
+++ scenes/gameover.py
@@ -1,9 +1,11 @@
 from base import BaseScene
+from screens.worlds import GameOverWorld
 
 class GameOverScene(BaseScene):
     """ You died, or planet was infected. Game over. """
 
-    def __init__(self, win_surf, FPS):
-        super(GameOverScene, self).__init__(win_surf, FPS)
+    def __init__(self, renderer):
+        self.renderer = renderer
+        super(GameOverScene, self).__init__(self.renderer.surf, self.renderer.fps)
         self.surfaces = GameOverWorld(self.surf.get_size()).surfaces
 
